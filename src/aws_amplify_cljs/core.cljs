4dbd2683d039bf624f6ddf5962bffd012d37243e
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-06-03 17:52:15 -0400

    Add backend

--- src/aws_amplify_cljs/core.cljs
+++ src/aws_amplify_cljs/core.cljs
@@ -1,21 +1,35 @@
 (ns aws-amplify-cljs.core
   (:require [reagent.core :as r]
-            ["aws-amplify" :default Amplify]
+            ["aws-amplify" :default Amplify :refer [Analytics API]]
             ["/aws-exports.js" :default aws-exports]))
 
-(.configure Amplify aws-exports)
+(defn get-items
+  []
+  (let [api-name "sampleCloudApi"
+        path "/items"]
+    (-> (.get API api-name path)
+        (.then #(js/console.log %))
+        (.catch #(js/console.error %)))))
+
+(defn items-btn
+  []
+  [:button {:on-click get-items} "Get Items"])
 
 (defn hello-world
   []
-  [:h1 "Hello, World!"])
+  [:div
+   [:h1 "Hello, World!"]
+   [items-btn]])
 
 (defn mount
   []
+  (.configure Amplify aws-exports)
+  (.record Analytics #js {:name "appRender"
+                          :attributes {:component "hello-world"}})
   (r/render [hello-world] (js/document.getElementById "app")))
 
 (mount)
 
 ;; Next steps:
-;; - Play with making amplify calls (e.g. Pinpoint analytics)
 ;; - Try writing some Lambda functions in cljs and calling them from the app
 ;; - Try loading data from a Datomic Cloud instance
